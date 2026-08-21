0906e03a5739f89c0fee7f538bf801e602e677e3
David Vera <dveracabanillas@gmail.com>
2026-08-20 20:02:23 -0500

    feat: agregar entidad pozo

---
+++ Electrobombas.Domain/Models/Pozo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Electrobombas.Domain.Models
+{
+    public class Pozo
+    {
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+        public decimal? Diametro { get; set; }
+        public decimal? Ne { get; set; }
+        public decimal? Profundidad { get; set; }
+        public decimal? CantidadTubos { get; set; }
+        public decimal? DiametroTubo { get; set; }
+        public decimal? Hp { get; set; }
+        public decimal? Voltaje { get; set; }
+        public decimal? Amperaje { get; set; }
+        public decimal? Rpm { get; set; }
+        public decimal? CaudalLps { get; set; }
+        public string? SerieMotor { get; set; }
+        public string? SerieBomba { get; set; }
+        public int? NumeroImpulsores { get; set; }
+        public int? IdUbicacion { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public DateTime? FechaActualizacion { get; set; }
+        public bool Estado { get; set; }
+    }
+}
